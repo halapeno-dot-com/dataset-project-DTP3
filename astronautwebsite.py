ca6a8cba417b1c1052d23734de9f02184ed3a37c
halapeno-dot-com <ff22236@student.phs.school.nz>
2026-08-19 17:26:21 +1200

    search can access the database, needs to be able to discern results

--- astronautwebsite.py
+++ astronautwebsite.py
@@ -26,29 +26,22 @@ def query_db(query, args=(), one=False):
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
-@app.route('/')
-def home():
-    #home page - astronaut name, ID
+def get_astronauts():
+#home page - astronaut name, ID
     sql = """
             SELECT astronauts.astronautID, astronauts.name,
             missions.missionID, missions.mission_name FROM astronauts 
             JOIN missions ON missions.missionID=astronauts.astronautID
             JOIN selections ON selections.selectionID=astronauts.astronautID;"""
     results = query_db(sql)
-    query = request.args.get('q', '')
-    search_result = None
-    if query:
-        query = query.lower().strip
-        for astronaut in results:
-            astronaut_name = astronaut[1].lower()
-            if query in astronaut_name:
-                search_result = astronaut
-                break
-    search_sql = "SELECT * FROM astronauts WHERE name LIKE?"
-    search_result = query_db(search_sql, ('%' + query + '%',), one=True)
+    return results
 
-    return render_template("Home.html", astronauts=results, search_result=search_result, query=query)
-
+@app.route('/')
+def home():
+    results = get_astronauts()
+ 
+    return render_template("Home.html", astronauts=results, search_result=None, query="")
+    
 @app.route("/astronauts/<int:id>")
 def astronaut(id):
     #one astronaut based on the ID
@@ -59,6 +52,21 @@ def astronaut(id):
              WHERE astronauts.astronautID = ?;"""
     result = query_db(sql,(id,),True)
     return render_template("Astronauts.html", astronaut=result)
+
+@app.route("/search")
+def search():
+   results = get_astraunauts()
+   query = request.args.get('q', '')
+   search_result = None
+   if query:
+        query = query.lower().strip()
+        for astronaut in results:
+            astronaut_name = astronaut[1].lower()
+            if query in astronaut_name:
+                search_result = astronaut
+                break
+            return render_template("Home.html", astronauts=results, search_result=search_result, query=query)        
+
 
 @app.route("/credits")
 def credits():
